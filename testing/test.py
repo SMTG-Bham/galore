3d0c099ad8014e9636070d4dc33d078dd8052e88
Adam J. Jackson <a.j.jackson@physics.org>
2016-07-22 16:45:43 +0100

    fix tests

--- testing/test.py
+++ testing/test.py
@@ -3,6 +3,7 @@ import numpy.testing
 import numpy as np
 import galore
 
+
 class test_array_functions(numpy.testing.TestCase):
     # The Numpy unit testing framework is used as this provides
     # comparison tools for ndarrays
@@ -11,11 +12,10 @@ class test_array_functions(numpy.testing.TestCase):
         self.assertEqual(galore.delta(1, 1.5, w=1), 1)
 
     def test_xy_to_1s(self):
-        self.assert_array_equal(galore.xy_to_1d(np.array([[2.1, 0.6],
-                                                       [4.3, 0.2],
-                                                       [5.1, 0.3]]),
-                                             1),
-                          np.array([0., 0., 0.6, 0., 0.2, 0.3]))
+        np.testing.assert_array_equal(
+            galore.xy_to_1d(np.array([[2.1, 0.6], [4.3, 0.2], [5.1, 0.3]]), 1),
+            np.array([0., 0., 0.6, 0., 0.2, 0.3]))
+
 
 if __name__ == '__main__':
     unittest.main()
