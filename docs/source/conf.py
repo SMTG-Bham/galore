26d0563f572024c8857f348af8ebd241fbe21b00
Adam J. Jackson <a.j.jackson@physics.org>
2017-09-29 14:35:44 +0100

    DOC: Fix RTD theme trailing lines in included code

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -101,6 +101,10 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
+# Load a little CSS to fix a bug in the RTD theme
+def setup(app):
+    app.add_stylesheet('css/custom.css')
+
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
 #
