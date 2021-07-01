5300d6253402d2de3498fcef511720fd856908fe
Yaxuan Li <598309385@qq.com>
2021-07-01 03:47:01 +0100

    import is fixed

--- galore/__init__.py
+++ galore/__init__.py
@@ -26,12 +26,13 @@ from collections import OrderedDict
 from collections.abc import Sequence
 import logging
 
+import galore.formats
 from math import sqrt, log
 import numpy as np
 from scipy.interpolate import interp1d
 
-import galore.formats
-from galore.cross_sections import cross_sections_info
+
+from galore.cross_sections import get_cross_sections, cross_sections_info
 
 
 def auto_limits(data_1d, padding=0.05):
