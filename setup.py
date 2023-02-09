0435279a156ca4152553d9557f12a4aa7da40299
Sean Kavanagh <s.kavanagh19@imperial.ac.uk>
2023-02-09 16:53:08 +0000

    Remove `python=3.9` (`pymatgen` legacy version issue?)

--- setup.py
+++ setup.py
@@ -44,7 +44,6 @@ if __name__ == "__main__":
             'Programming Language :: Python :: 3',
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
             'Topic :: Scientific/Engineering :: Chemistry',
             'Topic :: Scientific/Engineering :: Physics'
             ],
