df8f7e9fe6802900db68c9eb0e18a2fdd0097273
Hashan Peiris <peiris.mdhc@gmail.com>
2023-08-08 14:32:27 -0400

    Changes to be committed: 	modified:   galore/__init__.py 	modified:   galore/cli/galore.py 	modified:   galore/cross_sections.py 	modified:   galore/formats.py 	modified:   test/test_plot.py
    This is becasue "ImportError: cannot import name 'OrderedDict'
    from 'collections.abc'" results from the previous commit.

--- galore/__init__.py
+++ galore/__init__.py
@@ -21,7 +21,7 @@
 
 from __future__ import print_function
 
-from collections.abc import OrderedDict
+from collections import OrderedDict
 from collections.abc import Sequence
 import logging
 from math import sqrt, log
