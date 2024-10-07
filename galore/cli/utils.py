b07f3179042db1a05e2e2b8c66b850ec2659d5fe
Adam J. Jackson <a.j.jackson@physics.org>
2024-10-07 12:15:54 +0100

    Match against a number, not a module...

--- galore/cli/utils.py
+++ galore/cli/utils.py
@@ -6,7 +6,7 @@ from packaging.version import Version
 def get_default_style() -> str:
     """Get appropriate name for seaborn-colorblind, depending on MPL version"""
 
-    if Version(matplotlib) < Version("3.6"):
+    if Version(matplotlib.__version__) < Version("3.6"):
         return "seaborn-colorblind"
 
     return "seaborn-v0_8-colorblind"
